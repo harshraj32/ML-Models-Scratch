6c3af75011fd2715beadae397e0e4f27f18891c4
Harsh Raj J <harshraj201999@gmail.com>
2024-03-03 21:05:25 -0800

    improved time taken by a lot by using axis in np matrix multiplication in np array.

--- knn/KNN.py
+++ knn/KNN.py
@@ -1,3 +1,4 @@
+import math
 from collections import Counter
 
 import numpy as np
@@ -5,6 +6,10 @@ import numpy as np
 
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
+
+
+def manhattan_distance(x1, x2):
+    return np.sqrt(np.sum(math.abs(x1 - x2)))
 
 
 class KNN:
@@ -23,12 +28,12 @@ class KNN:
     def _predict(self, x):
 
         # compute the distance
-        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
-
+        # distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        distances = np.sqrt(np.sum((self.X_train - x) ** 2, axis=1))
         # get the closest k neighbors
         k_ind = np.argsort(distances)[: self.k]
         k_labels = [self.y_train[i] for i in k_ind]
 
         # get votes
-        most_common = Counter(k_labels).most_common()
+        most_common = Counter(k_labels).most_common(1)
         return most_common[0][0]
